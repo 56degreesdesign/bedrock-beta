d9f6522680979bcd7b2d7618121060b3f74bb80e
hiBartek <ewiakbartlomiej@gmail.com>
2023-09-04 13:51:32 +0200

    smooth-scroll update

--- web/app/themes/56degreesTheme/assets/js/utils/smooth-scroll.js
+++ web/app/themes/56degreesTheme/assets/js/utils/smooth-scroll.js
@@ -1,18 +1,31 @@
 module.exports = function () {
-    const links = document.querySelectorAll('a[href*="#"]:not([href="#"]):not([href="#0"])');
+    const links = document.querySelectorAll('a[href*="#"]:not([href="#"]):not([href="#0"])'),
+        header = document.querySelector(".header");
 
     for (const link of links) {
         link.addEventListener("click", clickHandler);
     }
 
     function clickHandler(e) {
-        e.preventDefault();
-        const href = this.getAttribute("href");
-        const offsetTop = document.querySelector(href).offsetTop;
+        let href = this.getAttribute("href");
 
-        scroll({
-            top: offsetTop,
-            behavior: "smooth"
-        });
+        // Check in anchor is another page
+        if ( href.includes('/#') ) {
+            href = href.replace('/#', '#')
+        }
+        else {
+            e.preventDefault();
+
+            // Check is element on the page
+            if ( document.querySelector(href) ) {
+                let offsetTop = document.querySelector(href)?.getBoundingClientRect().top - window.pageYOffset;
+                offsetTop = header ? ( offsetTop - header.getBoundingClientRect().height) : offsetTop;
+
+                scroll({
+                    top: offsetTop,
+                    behavior: "smooth"
+                });
+            }
+        }
     }
 };
