b3fad8d6421ec72fbcb9178ed474eed93b726421
hiBartek <86689320+hiBartek@users.noreply.github.com>
2024-07-19 17:23:22 +0200

    Update smooth-scroll.js

--- web/app/themes/56degreesTheme/assets/js/utils/smooth-scroll.js
+++ web/app/themes/56degreesTheme/assets/js/utils/smooth-scroll.js
@@ -1,4 +1,4 @@
-module.exports = function ($) {
+module.exports = function () {
     const links = document.querySelectorAll('a[href*="#"]:not([href="#"]):not([href="#0"])'),
         header = document.querySelector(".header");
 
@@ -9,22 +9,20 @@ module.exports = function ($) {
     function clickHandler(e) {
         let href = this.getAttribute("href");
 
-        // Check in anchor is another page
-        if ( href.includes('/#') ) {
-            if ( href.split("#")[0] === window.location.href.split("#")[0] ) {
+        try {
+            const url = new URL(href);
+            const siteUrl = window.location;
+
+            if ( siteUrl.origin === url.origin && siteUrl.pathname === url.pathname ) {
                 e.preventDefault();
-                moveTo(href.slice(href.indexOf("#")));
+                moveTo(url.hash);
             }
-        }
-        else {
-            e.preventDefault();
-            moveTo(href);
-        }
+        } catch (e) {}
     }
 
-    function moveTo(href) {
-        if ( document.querySelector(href) ) {
-            const element = document.querySelector(href);
+    function moveTo(hash) {
+        if ( document.querySelector(hash) ) {
+            const element = document.querySelector(hash);
             let offsetTop = element.getBoundingClientRect().top + window.scrollY;
             // offsetTop = header ? ( offsetTop - header.getBoundingClientRect().height) : offsetTop;
 
