0dd7b0fa2890d909e7650b001c58d8ad85eff8fe
hiBartek <86689320+hiBartek@users.noreply.github.com>
2024-08-14 18:11:41 +0200

    auto-sections get global or local data

--- web/app/themes/56degreesTheme/includes/utils/auto-sections.php
+++ web/app/themes/56degreesTheme/includes/utils/auto-sections.php
@@ -12,6 +12,12 @@ function get_auto_sections($name, $url)
 
             if (file_exists($path)) {
                 $data = get_sub_field($templateName);
+                
+                // get section local or global
+                if ( isset($data['scope_type']) ) {
+                    $data = $data['scope_type'] ? get_field($templateName, 'option')[$templateName] : $data['section'][$templateName];
+                }
+                
                 echo "<!-- " . $fileName . " -->";
                 get_template_part($url . '/' . $fileName, null, ['data' => $data]);
             } else {
